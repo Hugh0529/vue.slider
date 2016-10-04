b10b3f6a22bf46a4210c0a3b0830ffa8223982b3
chenhaoyang <chenhaoyang0529@gmail.com>
2016-10-04 18:58:25 +0800

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './demo/src/main.js',
+  output: {
+    path: './demo/static',
+    publicPath: '/demo/static/',
+    filename: 'build.js'
+  },
+  module: {
+    // avoid webpack trying to shim process
+    noParse: /es6-promise\.js$/,
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        // excluding some local linked packages.
+        // for normal use cases only node_modules is needed.
+        exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+        loader: 'babel!eslint'
+      }
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
+}
